248e89dcab5c8c8b1ae62f5033333c77cab2a8bb
Attila <attila.kisivan@gmail.com>
2022-07-11 21:38:29 +0100

    add cart and dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,9 @@ import userEvent from "@testing-library/user-event";
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as TdbLogo } from "../../assets/tdb.svg";
 import { UserContext } from "../../contexts/user.context";
 
@@ -32,7 +35,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
